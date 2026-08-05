8f05c292e8469087cbb565afdfb4f10ab91efb0c
Tiago Toledo <jtiagotoledo@gmail.com>
2026-08-05 14:49:48 -0300

    add texto inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="bg-black flex-1">
+    <div className="bg-black flex-1 justify-center flex p-5">
       <text className="text-white">Site Plutão Física</text>
     </div>
   );
