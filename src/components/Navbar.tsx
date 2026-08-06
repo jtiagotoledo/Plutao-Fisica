96445182e2b6bb21713d0f0863bf4ee91bf80e8f
Tiago Toledo <jtiagotoledo@gmail.com>
2026-08-06 14:48:32 -0300

    add item de menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
-export default function Navbar(){
-    return(
+import { Menu } from "lucide-react";
+
+export default function Navbar() {
+    return (
         <header className="h-16 bg-zinc-950 flex items-center justify-between px-6 border-b border-zinc-800">
-            <span className="text-zinc-100">Plutão Física</span>
+            <div className="flex">
+                <Menu className="w-6 h-6 text-amber-500 shrink-0 mr-6" />
+                <span className="text-zinc-100">Plutão Física</span>
+            </div>
         </header>
     );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,12 @@
 export default function SideBar(){
     return(
-        <aside className="hidden md:block w-64 p-4 shrink border-r border-zinc-800 bg-zinc-900">
-            <span>Side Bar</span>
+        <aside className="hidden md:block w-64 p-4 border-r border-zinc-800 bg-zinc-900 shrink-0">
+            <p className="text-zinc-100 text-xs font-semibold uppercase mb-4">Menu</p>
+            <nav className="space-y-2">
+                <a href="#" className="text-zinc-100 block px-3 py-2 rounded-lg bg-zinc-800 hover:bg-zinc-500">Envio de tarefa</a>
+                <a href="#" className="text-zinc-100 block px-3 py-2 rounded-lg bg-zinc-800 hover:bg-zinc-500">Histórico de envios</a>
+                <a href="#" className="text-zinc-100 block px-3 py-2 rounded-lg bg-zinc-800 hover:bg-zinc-500">Avisos</a>
+            </nav>
         </aside>
     );
 }
